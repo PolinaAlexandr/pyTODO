5565f59fa679f3732c0397c0a570403852e24d64
PolinaAlexandr <polinaalexnder@gmail.com>
2019-01-28 15:10:35 +0300

    Added Task class and modified basic functions

---
+++ task.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+class Task:
+
+
+    def __init__(self, name, note, deadline, id=None, started=datetime.now()):
+        self.name = name
+        self.note = note
+        self.deadline = deadline
+        self.started = started
+        self.id = id
+
+    def print(self):
+        print("| {:2} | {:<16} | {:<55} | {:30} | {:<30}".format(self.id, self.name, self.note, self.started, self.deadline).lower())
+
+
+
